b8c798d848269ac8e60085279f70c38cbd205a93
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-08-25 21:51:56 -0300

    Inicializado com eclipse

---
+++ 2-Erros/Erro01.java
@@ -0,0 +1,5 @@
+class Erro01 {
+    public static void main (String[] args) {
+        System.out.println("Hello world com erro!") // Erro provocado pela virgula (Erro de sintaxe)
+    }
+}
---
+++ 2-Erros/Erro03.java
@@ -0,0 +1,10 @@
+// O java é case Sensitive
+class Erro03 {
+    public static void Main (String[] args) { // O programa é compilado mas ao inicializar o programa não é encontrado a classe main que é o ponto de entrada do mesmo
+
+
+
+
+        System.out.println("Voce digitou: Nada");
+    }
+}
