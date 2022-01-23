688e37dfa8435ea589024c5986ec9bf397d9c237
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2022-01-22 22:33:29 -0300

    renew course only with udemy platform

--- 1-ArgumentosPrograma/Argumentos2.java
+++
@@ -1,6 +0,0 @@
-class Argumentos2 {
-    public static void main(String[] args){
-
-        System.out.println("Ola mundo " + args[0] + " e aqui vai o outro argumento " + args[1]);
-    }
-}
--- 2-Erros/Erro02.java
+++
@@ -1,9 +0,0 @@
-class Erro02 {
-    public static void main (String[] args) {
-
-        // int cont;  Erro provocado por não inicialização da variavel (Erro de variavel não inicializada)
-        int cont = 0; // Inicializei a variavel com o valor de 0
-
-        System.out.println("Voce digitou: " + cont);
-    }
-}
