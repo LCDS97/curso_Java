8914c9d2a58615fbe3cc46e58b3771033124be41
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2022-01-23 22:08:25 -0300

    class TipoString

---
+++ eclipse/udemy/Secao01-aulaFundamentos/src/exercicios/Aula06/Import.java
@@ -0,0 +1,17 @@
+package exercicios.Aula06;
+
+import java.util.Date; // Importado o Date util do Java para a classe
+
+import javax.swing.JButton;
+
+public class Import {
+	public static void main (String [] args) {
+		String s = "Eae!";
+		System.out.println(s);
+		
+		Date d = new Date(); // Importando estrutura Date do Java para a classe
+		System.out.println(d);
+		
+		JButton botao = new JButton();
+	}
+}
